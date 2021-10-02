191197a5ff0b5f75ffd1455d582ca58cdac3e6f5
Manan Gadwal <manan2000gadwal@gmail.com>
2021-10-02 13:15:03 +0530

    screens

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:signature/screens/generate.dart';
+import 'package:signature/screens/saved.dart';
 
 class home extends StatelessWidget {
   const home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome"),
+            backgroundColor: Colors.cyan,
+            centerTitle: true,
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  child: Text("Generator"),
+                ),
+                Tab(
+                  child: Text("Saved"),
+                ),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [generator(), saved()],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/saved.dart
+++ lib/screens/saved.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class generator extends StatelessWidget {
-  const generator({Key? key}) : super(key: key);
+class saved extends StatelessWidget {
+  const saved({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
