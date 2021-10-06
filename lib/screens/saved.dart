9a2b98e7a59bfea6b286bee7e8c4be4ee8013c53
Manan Gadwal <manan2000gadwal@gmail.com>
2021-10-06 12:56:21 +0530

    new saved

--- lib/screens/saved.dart
+++ lib/screens/saved.dart
@@ -1,28 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:signature/screens/addtodo.dart';
 
 class Saved extends StatelessWidget {
   const Saved({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => showDialog(
-              useSafeArea: true,
-              context: context,
-              builder: (BuildContext context) {
-                return AddTodoDialogeWidget();
-              },
-              barrierDismissible: true),
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-          backgroundColor: Colors.black,
-          splashColor: Colors.red,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18))),
-    );
+    return Container();
   }
 }
