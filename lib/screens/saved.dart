dbf618280ff5fb33546bd3e5fb267ae339f6e7f1
Manan Gadwal <manan2000gadwal@gmail.com>
2021-10-07 20:10:53 +0530

    new ui on saved page

--- lib/screens/saved.dart
+++ lib/screens/saved.dart
@@ -5,6 +5,45 @@ class Saved extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(21)),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                    title: Text('Save Password'),
+                    actions: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8, right: 8),
+                        child: TextFormField(
+                          decoration: InputDecoration(label: Text("Title")),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8, right: 8),
+                        child: TextFormField(
+                          decoration: InputDecoration(label: Text("Password")),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Center(
+                          child: IconButton(
+                              splashColor: Colors.red,
+                              onPressed: () {},
+                              icon: Icon(Icons.check)))
+                    ],
+                  ));
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black,
+        splashColor: Colors.red,
+      ),
+    );
   }
 }
