9c3fe8eab1091fcd6f49c74d63d6aa912a7b636a
Gaurav Yadav <90178033+GauravCalidig@users.noreply.github.com>
2021-10-23 09:04:07 +0530

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:signature/screens/login_screen.dart';
 import 'package:signature/screens/routes.dart';
 
-void main() {
+import 'screens/homepage.dart';
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -13,12 +20,11 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
         defaultTransition: Transition.fadeIn,
-        home: LoginScreen(),
+        home: auth.currentUser != null ? const Home() : const LoginScreen(),
         theme: ThemeData(
             brightness: Brightness.dark,
             primaryColor: Colors.red,
             accentColor: Colors.red),
-        initialRoute: MyRoutes.loginRoute,
         routes: routes);
   }
 }
--- lib/screens/saved.dart
+++ lib/screens/saved.dart
@@ -1,11 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Saved extends StatelessWidget {
+import '../main.dart';
+
+class Saved extends StatefulWidget {
   const Saved({Key? key}) : super(key: key);
+
+  @override
+  State<Saved> createState() => _SavedState();
+}
+
+class _SavedState extends State<Saved> {
+  final passController = TextEditingController();
+  final titleController = TextEditingController();
+  Stream<QuerySnapshot<Map<String, dynamic>>> ownerData() {
+    return FirebaseFirestore.instance
+        .collection("accounts")
+        .doc(auth.currentUser!.uid)
+        .collection("passwords")
+        .snapshots();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+          stream: ownerData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            print(snapshot.data!.docs);
+            if (!snapshot.hasData) {
+              return const Text("Error, try again ltaer");
+            }
+            if (snapshot.hasData) {
+              return snapshot.data!.docs.isEmpty
+                  ? Center(child: Text("No Saved Passwords"))
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        var passData = snapshot.data!.docs[index].data() as Map;
+                        return ListTile(
+                          leading: const Icon(Icons.lock),
+                          trailing: IconButton(
+                            onPressed: () async {
+                              await FirebaseFirestore.instance
+                                  .collection("accounts")
+                                  .doc(auth.currentUser!.uid)
+                                  .collection("passwords")
+                                  .doc(snapshot.data!.docs[index].id)
+                                  .delete();
+                            },
+                            icon: Icon(Icons.delete),
+                          ),
+                          title: Text(passData["title"] ?? ""),
+                          subtitle: Text(passData["password"] ?? ""),
+                        );
+                      });
+            }
+            return Container();
+          }),
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(21)),
         onPressed: () {
@@ -17,6 +73,7 @@ class Saved extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.only(left: 15, right: 15),
                         child: TextFormField(
+                          controller: titleController,
                           decoration: InputDecoration(label: Text("Title")),
                         ),
                       ),
@@ -26,6 +83,7 @@ class Saved extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.only(left: 15, right: 15),
                         child: TextFormField(
+                          controller: passController,
                           decoration: InputDecoration(label: Text("Password")),
                         ),
                       ),
@@ -35,7 +93,11 @@ class Saved extends StatelessWidget {
                       Center(
                           child: IconButton(
                               splashColor: Colors.red,
-                              onPressed: () {},
+                              onPressed: () {
+                                savePassword(
+                                    titleController.text, passController.text);
+                                Navigator.pop(context);
+                              },
                               icon: Icon(Icons.check))),
                       SizedBox(
                         height: 10,
@@ -48,5 +110,23 @@ class Saved extends StatelessWidget {
         splashColor: Colors.red,
       ),
     );
+  }
+
+  savePassword(String title, String pass) async {
+    var db = FirebaseFirestore.instance
+        .collection("accounts")
+        .doc(auth.currentUser!.uid)
+        .collection("passwords");
+
+    Map<String, dynamic> data = {
+      "title": title,
+      "password": pass,
+      "timeStamp": FieldValue.serverTimestamp()
+    };
+
+    await db.doc(title + pass).set(data).then((value) {
+      passController.clear();
+      titleController.clear();
+    });
   }
 }
