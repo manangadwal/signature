62bd48e6a90d65329a5001b39bd0391fdc641688
Gaurav Yadav <90178033+GauravCalidig@users.noreply.github.com>
2021-12-03 10:32:57 +0530

    minor

--- lib/screens/bottom_navbar.dart
+++ lib/screens/bottom_navbar.dart
@@ -19,7 +19,12 @@ class _BottomNavBarState extends State<BottomNavBar> {
   int index = 0;
   @override
   Widget build(BuildContext context) {
-    List<Widget> tabs = [const Home(), Container(), Container(), Settings()];
+    List<Widget> tabs = [
+      const Home(),
+      Container(child: Center(child: Text("Coming Soon"))),
+      Container(child: Center(child: Text("Coming Soon"))),
+      const Settings()
+    ];
     return Scaffold(
       body: tabs[index],
       bottomNavigationBar: Container(
